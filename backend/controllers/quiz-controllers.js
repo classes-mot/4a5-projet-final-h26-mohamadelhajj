a5ab8202579ab31b91aa6bee7b250e8f966a35d6
Mohamad El-Hajj <2496552@cmontmorency.qc.ca>
2026-04-24 08:50:35 -0400

    FAIT :  quiz-controllers fini mais pas tester

--- backend/controllers/quiz-controllers.js
+++ backend/controllers/quiz-controllers.js
@@ -1,5 +1,6 @@
 import { Quiz } from "../models/quiz.js";
 import { User } from "../models/user.js";
+import { Question } from "../models/question.js";
 import HttpError from "../utils/http-error.js";
 import { validationResult } from "express-validator";
 import mongoose from "mongoose";
@@ -64,9 +65,48 @@ const createQuiz = async (req, res, next) => {
   res.status(201).json({ quiz: createdQuiz });
 };
 
-const updateQuiz = async (req, res, next) => {};
+const updateQuiz = async (req, res, next) => {
+  const quizId = req.params.qid;
+  const quizUpdates = req.body;
 
-const deleteQuiz = async (req, res, next) => {};
+  try {
+    const quiz = await Quiz.findById(quizId);
+
+    if (!quiz) {
+      return res.status(404).json({ message: "Quiz non trouvé" });
+    }
+
+    quiz.updateOne(quizUpdates);
+
+    const updatedQuiz = await Quiz.findById(quizId).populate("user");
+
+    res.status(200).json({ quiz: updatedQuiz.toObject({ getters: true }) });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Erreur lors de la mis à jour du jeu." });
+  }
+};
+
+const deleteQuiz = async (req, res, next) => {
+  const quizId = req.params.qid;
+
+  try {
+    const quiz = await Quiz.findById(quizId).populate("user");
+
+    if (!quiz) {
+      return res.status(404).json({ message: "Quiz non trouvé" });
+    }
+    await Question.deleteMany({ quiz: quizId });
+    await quiz.deleteOne();
+    quiz.user.quiz.pull(quiz._id);
+    await quiz.user.save();
+
+    res.status(200).json({ message: "Quiz supprimé" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Erreur lors de la suppression du jeu." });
+  }
+};
 
 export default {
   getQuizByUserId,
