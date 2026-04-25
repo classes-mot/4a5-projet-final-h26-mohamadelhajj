ece14cb8a5dcbd7ab12fafe9b1fd94338347c444
Mohamad El-Hajj <2496552@cmontmorency.qc.ca>
2026-04-25 16:07:11 -0400

    FAIT : ajout de validation pour quiz

--- backend/routes/quiz-routes.js
+++ backend/routes/quiz-routes.js
@@ -1,12 +1,17 @@
 import express from "express";
 import quizControllers from "../controllers/quiz-controllers.js";
+import { check } from "express-validator";
 import checkAuth from "../middleware/check-auth.js";
 
 const router = express.Router();
 
 router.get("/getQuiz/:uid", quizControllers.getQuizByUserId);
 
-router.post("/newQuiz", [checkAuth], quizControllers.createQuiz);
+router.post(
+  "/newQuiz",
+  [checkAuth, check("titre").not().isEmpty()],
+  quizControllers.createQuiz,
+);
 
 router.patch("/updateQuiz/:qid", [checkAuth], quizControllers.updateQuiz);
 
